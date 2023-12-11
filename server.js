89d0afd78d3d36ee27be9b4bd46a3c9cf6f363af
Umut Yenidil <umut.yenidil9@gmail.com>
2023-12-12 00:52:18 +0300

    server icin kurulum yapildi

--- server.js
+++ server.js
@@ -1,7 +1,29 @@
-const express = require('express');
+// packages
+import express, {json} from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
+import {dirname, join} from 'path';
+import {fileURLToPath} from 'url';
 
+// env config
+dotenv.config();
+
+// variables
 const app = express();
+const PORT = process.env.PORT || 5000;
+const corsOptions = {credentials: true, origin: process.env.URL || "*"};
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
-app.listen(3000, (error)=>{
-    console.log(error);
+// package middlewares
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(cookieParser());
+
+// file definition middlewares
+app.use('/', express.static(join(__dirname, 'public')));
+
+// listening
+app.listen(PORT, (error) => {
+    console.log(`Server is listening on ${PORT}`);
 });
